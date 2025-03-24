51daf2e8c0e6d857e5e0b3ede9ef6748c2f8d7d8
VrezenStrijder <f94fbr@outlook.com>
2025-03-24 21:43:56 +0800

    Create SysUtil.cs

---
+++ Utility/SysUtil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using Microsoft.VisualBasic;
+
+namespace Layouter.Utility
+{
+    public class SysUtil
+    {
+        public static IntPtr GetWindowHandle(Window win)
+        {
+            IntPtr hwnd = new WindowInteropHelper(win).Handle;
+            return hwnd;
+        }
+
+        public static bool IsWindows11()
+        {
+            bool? flag = null;
+
+            if (flag == null)
+            {
+                try
+                {
+                    var osVersion = Environment.OSVersion.Version;
+                    if (osVersion.Major >= 10 && osVersion.Build >= 22000)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        using (var searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem"))
+                        {
+                            foreach (var os in searcher.Get())
+                            {
+                                string caption = os["Caption"].ToString();
+                                flag = caption.Contains("Windows 11");
+                                break;
+                            }
+                        }
+
+                        if (flag == null)
+                        {
+                            flag = (osVersion.Major == 10 && osVersion.Build >= 22000);
+                        }
+                    }
+                }
+                catch
+                {
+                    flag = false;
+                }
+            }
+
+            return flag.Value;
+        }
+
+    }
+}
