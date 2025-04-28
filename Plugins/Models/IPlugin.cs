58577064d19bc496b7bc4028b9e7f3688ae3cdcf
VrezenStrijder <f94fbr@outlook.com>
2025-04-28 23:33:52 +0800

    plugin model

---
+++ Plugins/Models/IPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Layouter.Plugins
+{
+    //public interface IPlugin
+    //{
+    //    string Name { get; }
+    //    void Load();
+    //    IReadOnlyDictionary<string, Action<object>> GetFunctions();
+    //    void Run(string functionKey, object arg);
+    //}
+
+    public interface IPlugin
+    {
+        string Name { get; }
+        void Load();
+        IReadOnlyDictionary<string, Func<dynamic, object>> GetFunctions();
+        object Run(string functionKey, dynamic parameters);
+
+        /// <summary>
+        /// 获取参数描述
+        /// </summary>
+        Dictionary<string, List<ParameterInfo>> GetParameterDescriptions();
+    }
+
+
+}
---
+++ Plugins/Models/IconDescriptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Layouter.Plugins.Models
+{
+    public class IconDescriptor
+    {
+        private Dictionary<string, string> _iconPaths = new Dictionary<string, string>();
+
+        public IReadOnlyDictionary<string, string> IconPaths => _iconPaths;
+
+        public static IconDescriptor FromJson(string json)
+        {
+            var descriptor = new IconDescriptor();
+            descriptor._iconPaths = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+            return descriptor;
+        }
+    }
+
+}
