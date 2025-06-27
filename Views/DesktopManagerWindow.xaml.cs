f7f9760995ac078f1fc8129f31ba9359744740fe
VrezenStrijder <f94fbr@outlook.com>
2025-06-27 21:41:32 +0800

    Update DesktopManagerWindow.xaml.cs

--- Views/DesktopManagerWindow.xaml.cs
+++ Views/DesktopManagerWindow.xaml.cs
@@ -95,12 +95,7 @@ namespace Layouter.Views
 
         private void DesktopManagerWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            IntPtr hwnd = SysUtil.GetWindowHandle(this);
-
-            //DesktopUtil.SetAsDesktopLevelWindow(hwnd);
-            DesktopUtil.SetShellViewAsOwnerWindow(hwnd); //worked!
-
-            DesktopUtil.SetAsToolWindow(hwnd);
+            SysUtil.SetDesktopLevelWindow(this);
 
             // 加载分区数据
             LoadPartitionData();
